439b3d6ada75e4b190414361d2458aff924c02d3
nikhilpounikar <nikhilpounikar@gmail.com>
2023-08-11 13:32:58 +0530

    Batch added

--- controller/batch_controller.js
+++ controller/batch_controller.js
@@ -5,6 +5,8 @@ module.exports.createBatch = function(req,res){
 
   return res.render('batch_form',{title:"Placement Cell | Batch"});
 }
+
+
 module.exports.allBatches = async (req, res) => {
     try {
       const availableBatches = await Batch.find(); // Fetch available batches from your database
@@ -15,4 +17,27 @@ module.exports.allBatches = async (req, res) => {
       console.error(error);
       res.status(500).send("Internal Server Error");
     }
+}
+
+module.exports.addBatch = async function(req,res){
+
+  try {
+    const batch = await Batch.findOne({name:req.body.name}); // Fetch available batches from your database
+    
+    if(batch){
+      req.flash('error','Batch Already Exits');
+      return res.redirect("back");
+    }
+
+    await Batch.create(req.body);
+    req.flash('success','Batch Added Successfully');
+    return res.redirect("/");
+  } catch (error) {
+    // Handle error
+    console.error(error);
+    //res.status(500).send("Internal Server Error");
+    req.flash('error','Internal Server Error');
+    return res.redirect("back");
+  }
+
 }
--- routes/batch_route.js
+++ routes/batch_route.js
@@ -6,5 +6,6 @@ const batchController = require("../controller/batch_controller");
 const router = express.Router();
 
 router.get("/create-batch", batchController.createBatch);
+router.post("/create-batch", batchController.addBatch);
 
 module.exports = router;
