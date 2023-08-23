e6abf9b91241af0b377bc0a78d20e2e844ffcd16
nikhilpounikar <nikhilpounikar@gmail.com>
2023-08-23 08:41:34 +0530

    index.js comments added

--- index.js
+++ index.js
@@ -1,57 +1,59 @@
+// Import required modules
 const cookieParser = require("cookie-parser");
 const express = require("express");
 const path = require("path");
 
+// Create an instance of the Express application
 const app = express();
 
 const port = 8000;
 const layouts = require("express-ejs-layouts");
 
+// Import the database configuration
 const db = require("./config/mongoose");
 
-//used for session cookie
-const session = require("express-session");
-const passport = require("passport");
+// Import passport local strategy
 const passportLocal = require("./config/passport_local_strategy");
 
+// Import bodyParser for parsing request bodies
 const bodyParser = require('body-parser');
 
+// Use JSON body parser for handling JSON data in requests
 app.use(bodyParser.json());
-//get mongostore config
-const MongoStore = require("connect-mongo")(session);
 
+// Set up static file serving for assets
+app.use(express.static('./assets'));
 
-const flash = require("connect-flash");
-const customMWare = require("./config/middleware");
-
-
-app.use(cookieParser());
-app.use(express.urlencoded({ extended: false }));
-app.use(express.static('./assests'));
-//puts styles and script to respective postion i.e in head and end the body respectively
+// Set up EJS layouts for views
 app.set("layout extractStyle ", true);
 app.set("layout extractScripts ", true);
-// Layouts Should be rendered before routing
-
 app.use(layouts);
-// For using the file in assets folder.
-app.use(express.static('./assets'));
-//set up the view engine
+
+// Set the view engine and views directory
 app.set("view engine", "ejs");
 app.set("views", "./views");
 
-//mongo store is being used to store session cookie in db
+// Set up various middleware
+
+// Parse cookies using cookie-parser
+app.use(cookieParser());
+
+// Parse URL-encoded request bodies
+app.use(express.urlencoded({ extended: false }));
+
+// Session setup using express-session
+const session = require("express-session");
+const MongoStore = require("connect-mongo")(session);
+
 app.use(
   session({
     name: "Placement Cell",
-    // TODO change the secret before deployment in production mode
-    secret: 'nikhil_pounikar',
+    secret: 'nikhil_pounikar', // TODO: Change the secret before deployment
     saveUninitialized: false,
     resave: false,
     cookie: {
       maxAge: 1000 * 60 * 100,
     },
-
     store: new MongoStore(
       {
         mongooseConnection: db,
@@ -64,20 +66,29 @@ app.use(
   })
 );
 
+// Initialize passport for authentication
+const passport = require("passport");
 app.use(passport.initialize());
 app.use(passport.session());
+
+// Middleware to set the authenticated user in the request
+const customMWare = require("./config/middleware");
 app.use(passport.setAuthenticatedUser);
+
+// Middleware for flash messages
+const flash = require("connect-flash");
 app.use(flash());
 app.use(customMWare.setFlash);
 
-// let middleware handle initial routing
+// Set up the initial routing using index_routes
 app.use("/", require("./routes/index_routes"));
 
+// Start the server listener
 app.listen(port, function (err) {
   if (err) {
-    console.log("Error while starting an application : ", err);
+    console.log("Error while starting the application: ", err);
     return;
   }
 
-  console.log("Server Started on port : ", port);
+  console.log("Server started on port: ", port);
 });
